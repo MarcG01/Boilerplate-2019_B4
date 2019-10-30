856eb0dea438eca700f8d8764da555bff76b3b6d
MarcG01 <magrenier01@gmail.com>
2019-10-30 13:30:13 -0400

    Boilerplate avec @babel/preset-env

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ *  Ajout de Bootstrap
+**/
+import "bootstrap";
+
+import "../css/style.scss";
+
+
+/* --------------- CODE --------------- */
+console.log("--------------- JS ---------------");
+
+
+
+import {monCompteur, incrementeMonCompteur} from "./librairie";
+
+console.log(monCompteur);
+
+incrementeMonCompteur();
+
+console.log(monCompteur);
